ace532e2beefcfedfc3f5c1de1285c45f05ef031
Pierre <pierre.balland@edu.ece.fr>
2019-12-05 00:50:56 +0100

    Avancement sur front navbar + Side + graphs principaux centre

--- projet_web/front_react/src/App.js
+++ projet_web/front_react/src/App.js
@@ -1,38 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-const _ =require('bootstrap')
-class App extends React.Component {
+
+import Navbar from './Navbar.js';
+import Chart1 from './Chart1.js';
+import Pcircle from "./ProgressCircle";
+import Pie1 from './Pie.js';
+const _ =require('bootstrap');
 
 
 
-  render() {
+class Rendufinale extends React.Component{
+
+    render() {
+        return(
+            <div className="main">
+                <Navbar/>
+                <br/>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-9">
+
+                        <div id="mainCard" className="card" >
+                            <div className="row">
+                                <Chart1/>
+                            </div>
+
+                            <div className="row">
+                                <div className="col-lg-6">
+                                    <Pie1/>
 
 
-    return (
+                                </div>
+                                <div className="col-lg-6">
+                                    <Pcircle/>
+                                </div>
+                            </div>
 
-        <div id="globaldiv">
 
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-lg-3  col-sm-3 d-none d-lg-block">
-                        Bonjour 1
+
+                        </div>
+                        </div>
+                        <div className="col-lg-3">
+                            <div id="asideCard" className="card">
+                                <article>
+                                    Pollution par pays
+                                    France: 37
+                                </article>
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-lg-3  col-sm-3 d-none d-lg-block">
-                        Bonjour 2
-                    </div>
-                    <div className="col-lg-3  col-sm-3 d-none d-lg-block">
-                        Bonjour 3
-                    </div>
-                    <div className="col-lg-3  col-sm-3 d-none d-lg-block">
-                        Bonjour 4
-                    </div>
-                </div>
+
             </div>
-        </div>
-    );
-  }
+            </div>
 
+        );
+    }
 }
 
-export default App;
+export default Rendufinale;
